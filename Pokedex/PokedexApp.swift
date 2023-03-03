6339850db35c4fb383fa4ac013ac0a155b87f906
admin <carwolt123@gmail.com>
2023-03-03 17:20:17 +0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by iMac G1 on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
